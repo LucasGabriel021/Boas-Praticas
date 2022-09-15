7779e40f9fdc5514c4b1186c01c22ca33ef817ac
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-09-15 16:08:47 -0300

    Update generics.ts

--- TypeScript/generics.ts
+++ TypeScript/generics.ts
@@ -28,3 +28,18 @@ newState.get();
 newState.set('Sam');
 newState.set(021); // Erro
 newState.set('Saravia');
+
+// Teste 
+function pressId<V>(id: V) {
+    return id;
+}
+
+console.log(pressId<number>(3));
+
+function identidade<T, V>(nome: T, idade: V) {
+    return { nome, idade };
+}
+
+let resultado = identidade<string, number>('Lucas', 19)
+
+console.log(`Seu nome é ${resultado.nome} e sua idade é ${resultado.idade}`);
