c2024b9053e0ac55ae1136999c9eeb8e5ffd70e0
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-09-15 15:44:13 -0300

    Update generics.ts

--- TypeScript/generics.ts
+++ TypeScript/generics.ts
@@ -1,1 +1,30 @@
-// Generics
+// Utilização de Generics
+
+// Convensões de Generics
+/**
+ * E => Element
+ * K => Key
+ * S => state
+ * T => Type
+ * V => Value
+ */
+
+function useState<T extends string | number = number>() {
+    let state: T;
+
+    function get() {
+        return state;
+    }
+
+    function set(newValue: T) {
+        state = newValue;
+    }
+
+    return { get, set };
+}
+
+let newState = useState<string>();
+newState.get();
+newState.set('Sam');
+newState.set(021);
+newState.set('Saravia');
