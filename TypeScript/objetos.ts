fb7a3ba18a8ee6cd70146c15a31112f3aa586d7d
Lucas Gomes de Oliveira <lucasgabrielgomes33@gmail.com>
2022-09-15 16:42:56 -0300

    Update objetos.ts

--- TypeScript/objetos.ts
+++ TypeScript/objetos.ts
@@ -8,3 +8,19 @@ type UserResponse = {
 // O padrão do userResponse deve estar de acordo com o type criado!
 let userResponse = {} as UserResponse;
 userResponse.name;
+
+// Type Object
+type Crachar = {
+    id: number,
+    name: string,
+    cpf: number,
+    pet?: string
+}
+
+let preencher: Crachar = {
+    id: 7,
+    name: 'Reus',
+    cpf: 62888282
+}
+
+console.log(preencher);
